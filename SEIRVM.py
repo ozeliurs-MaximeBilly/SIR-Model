c52ba809a71767378cf0d0fcdb7a5b1dc1d742fa
Ozeliurs <ozeliurs@gmail.com>
2021-10-25 16:34:42 +0200

    Migration terminée vers notre moteur

--- SEIRVM.py
+++ SEIRVM.py
@@ -1,30 +1,55 @@
-#!/usr/bin/env python3
-
-import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
 from matplotlib.widgets import Slider
 
 # Configuration de l'affichage de matplotlib
-matplotlib.use('Qt5Agg')
-#matplotlib.use('TkAgg')
+# matplotlib.use('Qt5Agg') # A utiliser sur Linux
+matplotlib.use('TkAgg') # A utiliser sur Windows
+
+# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+INIT_ALPHA = 0.75  # Taux d'incubation (0-1)
+INIT_BETA = 0.8  # Taux de transmission (0-1)
+INIT_GAMMA = 0.05  # Taux de guérison (0-1)
+INIT_MICRO = 0.01  # Taux de mortalité (0-1)
+INIT_NU = 0.009  # Taux de natalité (0-0.5)
+INIT_EPSILON = 1.0  # Taux de vaccination (0-2)
+INIT_DELTA = 0.2  # Taux de mortalité lié a la maladie (0-1)
+
+# Populations initiales
+N0 = 1000  # Population
+E0 = 0  # Nombre initial de personnes infectées non-infectieuses
+I0 = 5  # Nombre initial de personnes infectées infectieuses
+R0 = 0  # Nombre initial de personnes retirées
+S0 = N0 - (I0 + R0 + E0)  # Nombre initial de personnes Saines
+V0 = 0  # Nombre initial de personnes vacinnées
+M0 = 0  # Nombre initial de personnes mortes liées a la maladie
+
+# Precision et durée de la simulation
+SIM_PRECISION = 1000
+SIM_MULTIPLIER = 2
 
 
-# --- Fonctions ---
-# Equations differentielles du modèle SEIR
-def deriv(_t, y, alpha, beta, gamma, micro, nu, tv, mv):
-    """
-    Methode regroupant les équations differentielles du modèle SEIR.
-    """
-    S, E, I, R, V, M= y
-    dSdt = -beta * S * I + nu * (S + E + I + R + V) - micro * S - tv*S
-    dEdt = beta * S * I - alpha * E - micro * E + beta*R
-    dIdt = alpha * E - ((gamma + micro) * I) - mv*I
-    dRdt = gamma * I  - micro * R  - tv*R -beta*R
-    dVdt = tv*S + tv*R - micro*V
-    dMdt = mv *I
-    return dSdt, dEdt, dIdt, dRdt, dVdt, dMdt
+def solve(S0, E0, I0, R0, V0, M0, alpha, beta, gamma, micro, nu, epsilon, delta):
+    S, E, I, R, V, M = [S0], [E0], [I0], [R0], [V0], [M0]
+    h = 1/SIM_PRECISION
+    for o in range(SIM_PRECISION*SIM_MULTIPLIER):
+        St, Et, It, Rt, Vt, Mt = S[o-1], E[o-1], I[o-1], R[o-1], V[o-1], M[o-1]
+
+        #Equations
+        dSdt = -beta*St*It + nu*(St+Et+It+Rt+Vt) - micro*St - epsilon*St
+        dEdt = beta*St*It - alpha*Et - micro*Et
+        dIdt = alpha*Et - gamma*It - micro*It - It*delta
+        dRdt = gamma*It - micro*Rt - epsilon*Rt
+        dVdt = epsilon*St + epsilon*Rt - micro*Vt
+        dMdt = It*delta
+
+        S.append(St+h * dSdt)
+        E.append(Et+h * dEdt)
+        I.append(It+h * dIdt)
+        R.append(Rt+h * dRdt)
+        V.append(Vt+h * dVdt)
+        M.append(Mt+h * dMdt)
+    return S, E, I, R, V, M
 
 
 # The function to be called anytime a slider's value changes
@@ -32,63 +57,32 @@ def update(_x):
     """
     Méthode appelée a chaque changement des sliders. Recalcule les courbes et les affiche.
     """
-    solution = solve_ivp(fun=deriv, t_span=(0, SIM_TIME), t_eval=t, dense_output=True, y0=(S0, E0, I0, R0, V0, M0), method='DOP853',
-                         args=(alpha_slider.val, beta_slider.val, gamma_slider.val, micro_slider.val, nu_slider.val, tv_slider.val, mv_slider.val))
-    S, E, I, R, V, M = solution.y
+    S, E, I, R, V, M = solve(S0, E0, I0, R0, V0, M0, alpha_slider.val, beta_slider.val, gamma_slider.val, micro_slider.val, nu_slider.val, epsilon_slider.val, delta_slider.val)
     line1.set_ydata(S)
-    line2.set_ydata(I)
-    line3.set_ydata(R)
-    line4.set_ydata(E)
+    line2.set_ydata(E)
+    line3.set_ydata(I)
+    line4.set_ydata(R)
     line5.set_ydata(V)
-    N = [S[i] + E[i] + I[i] + R[i] +V[i] for i in range(len(S))]
-    line6.set_ydata(N)
-    line7.set_ydata(M)
+    line6.sety_data(M)
+    N = [S[i] + E[i] + I[i] + R[i] + V[i] for i in range(len(S))]
+    line7.set_ydata(N)
     fig.canvas.draw_idle()
 
 
-# --- Paramètres Initiaux ---
-SIM_TIME = 50  # Simulation time
-SIM_PRECISION = 250  # Samples per day
+S, E, I, R, V, M = solve(S0, E0, I0, R0, V0, M0, INIT_ALPHA, INIT_BETA, INIT_GAMMA, INIT_MICRO, INIT_NU, INIT_EPSILON, INIT_DELTA)
+N = [S[i] + E[i] + I[i] + R[i] + V[i] for i in range(len(S))]
 
-N0 = 1000  # Population
-E0 = 0  # Nombre initial de personnes infectées non-infectieuses
-I0 = 5  # Nombre initial de personnes infectées infectieuses
-R0 = 0  # Nombre initial de personnes retirées
-V0 = 0  # Nombre initial de personnes vacinnées
-M0 = 0  # Nombre initial de personnes mortes
-S0 = N0 - (I0 + R0 + E0)  # Nombre initial de personnes Saines
-
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-INIT_ALPHA = 0.75  # Taux d'incubation (0-1)
-INIT_BETA = 0.1  # Taux de transmission (0-1)
-INIT_GAMMA = 0.05  # Taux de guérison (0-1)
-INIT_MICRO = 0.01  # Taux de mortalité (0-1)
-INIT_NU = 0.01  # Taux de natalité (0-0.5)
-INIT_TV = 0.1
-INIT_MV = 0.1
-
-# Une grille de points de temps (en jours)
-t = np.linspace(0, SIM_TIME, SIM_TIME * SIM_PRECISION)
-
-# Creation & Configuration d'un subplot pour l'affichage des courbes d'evolution
 fig, ax = plt.subplots()
 ax.margins(x=0)
-ax.autoscale(True)
 
-# Résolution des équations différentielles avec les paramètres Initiaux
-ret = solve_ivp(fun=deriv, t_span=(0, SIM_TIME), t_eval=t, dense_output=True, y0=(S0, E0, I0, R0, V0, M0), method='DOP853',
-                args=(INIT_ALPHA, INIT_BETA, INIT_GAMMA, INIT_MICRO, INIT_NU, INIT_TV, INIT_MV))
-S, E, I, R, V, M= ret.y
+line1, = plt.plot(S, label="Healthy")
+line2, = plt.plot(E, label="Exposed")
+line3, = plt.plot(I, label="Infected")
+line4, = plt.plot(R, label="Recovered")
+line5, = plt.plot(V, label="Vaccinated")
+line6, = plt.plot(M, label="Dead by Disease")
+line7, = plt.plot(N, label="Population")
 
-# Ajout des courbes d'évolution avec leurs labels
-line1, = plt.plot(S, label="Susceptible")
-line2, = plt.plot(I, label="Infected")
-line3, = plt.plot(R, label="Recovered with Immunity")
-line4, = plt.plot(E, label="Exposed")
-line5, = plt.plot(V, label= "Vaccinated")
-N = [S[i] + E[i] + I[i] + R[i] + V[i] for i in range(len(S))]
-line6, = plt.plot(N, label="Population")
-line7, = plt.plot(M, label="morts")
 # Ajustement des tracés principaux pour faire de la place aux sliders
 plt.subplots_adjust(left=0.1, bottom=0.4)
 ax.set_xlabel('Time [days]')
@@ -96,8 +90,8 @@ ax.legend()
 
 # Slider Horizontal alpha
 alpha_slider = Slider(
-    ax=plt.axes([0.1, 0.25, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
-    label='alpha (Incubation)',
+    ax=plt.axes([0.1, 0.31, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    label='α (Incubation)',
     valmin=0,
     valmax=1,
     valinit=INIT_ALPHA,
@@ -106,17 +100,17 @@ alpha_slider = Slider(
 
 # Slider Horizontal beta
 beta_slider = Slider(
-    ax=plt.axes([0.1, 0.20, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    ax=plt.axes([0.1, 0.26, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
     label='β (Transmission)',
     valmin=0,
-    valmax=0.05,
+    valmax=1,
     valinit=INIT_BETA,
     color="red"
 )
 
 # Slider Horizontal gamma
 gamma_slider = Slider(
-    ax=plt.axes([0.1, 0.15, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    ax=plt.axes([0.1, 0.21, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
     label='γ (Guérison)',
     valmin=0,
     valmax=1,
@@ -126,47 +120,50 @@ gamma_slider = Slider(
 
 # Slider Horizontal micro
 micro_slider = Slider(
-    ax=plt.axes([0.1, 0.10, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    ax=plt.axes([0.1, 0.16, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
     label='μ (Mortalité)',
     valmin=0,
-    valmax=0.013,
+    valmax=1,
     valinit=INIT_MICRO,
-    color="black"
+    color="purple"
 )
 
 # Slider Horizontal nu
 nu_slider = Slider(
-    ax=plt.axes([0.1, 0.05, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    ax=plt.axes([0.1, 0.11, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
     label='ν (Natalité)',
     valmin=0,
-    valmax=0.013,
+    valmax=0.5,
     valinit=INIT_NU,
-    color="white"
+    color="pink"
 )
 
-tv_slider = Slider(
-    ax=plt.axes([0.1, 0.30, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
-    label='tv (vaccination)',
+# Slider Horizontal epsilon
+epsilon_slider = Slider(
+    ax=plt.axes([0.1, 0.06, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    label='ϵ (Vaccination)',
     valmin=0,
-    valmax=0.13,
-    valinit=INIT_TV,
-    color="brown"
+    valmax=2,
+    valinit=INIT_EPSILON,
+    color="lightgreen"
 )
 
-mv_slider = Slider(
-    ax=plt.axes([0.1, 0.00, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
-    label='mv (mortalite du virus)',
+delta_slider = Slider(
+    ax=plt.axes([0.1, 0.01, 0.8, 0.03], facecolor="lightgoldenrodyellow"),
+    label='δ (Mortalité Maladie)',
     valmin=0,
-    valmax=0.13,
-    valinit=INIT_TV,
-    color="brown"
+    valmax=1,
+    valinit=INIT_DELTA,
+    color="black"
 )
+
 # register the update function with each slider
 alpha_slider.on_changed(update)
 beta_slider.on_changed(update)
 gamma_slider.on_changed(update)
 micro_slider.on_changed(update)
 nu_slider.on_changed(update)
-tv_slider.on_changed(update)
-mv_slider.on_changed(update)
+epsilon_slider.on_changed(update)
+delta_slider.on_changed(update)
+
 plt.show()
